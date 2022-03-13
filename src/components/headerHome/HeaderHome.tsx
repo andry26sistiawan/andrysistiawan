bb978e143f691d26c469793188e51011d3c1dc79
Andry Sistiawan <andry26sistiawan@gmail.com>
2022-03-13 23:04:09 +0700

    Css HeaderHome

--- src/components/headerHome/HeaderHome.tsx
+++ src/components/headerHome/HeaderHome.tsx
@@ -2,18 +2,27 @@ import React, { Component } from 'react';
 
 const HeaderHome = (props : any) => {
     return (
-        <div style={{display:'flex', }}>
-          <div style={{height:'18vh', width:'100%', display:'flex', position:'fixed', flex:1, flexDirection:'column',}}>
-            <div style={{height:80, width:'100%',justifyContent:'center', display:'flex', alignItems:'center', backgroundColor:'#06E752' }}>
-              <div style={{ textDecoration: 'none', color:'black' }}>
-                <div style={{marginRight:'5vh',height:50,  justifyContent:'center', alignItems:'center', display:'flex',}}>
-                    <h3 style={{color:'white'}}>{props.title}</h3>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div style={styles.container}>
+            <h3 style={styles.textTitle}>{props.title}</h3>
         </div>
     );
 }
+
+const styles = {
+    container: {
+        display:'flex', 
+        height:'14vh', 
+        justifyContent:'center', 
+        alignItems:'center', 
+        width:'100%', 
+        position:'fixed', 
+        flex:1, 
+        flexDirection:'column', 
+        backgroundColor:'#06E752'
+    },
+    textTitle : {
+        color:'white'
+    }
+  } as const;
 
 export default HeaderHome;
